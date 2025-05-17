92a5a1456c81c496502483df59a1f40a9a6470bc
nGyu <vpdls1511@gmail.com>
2025-05-17 14:38:02 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/logisim/LogisimApplication.java
@@ -0,0 +1,13 @@
+package com.logisim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisimApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LogisimApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/logisim/LogisimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logisim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisimApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
