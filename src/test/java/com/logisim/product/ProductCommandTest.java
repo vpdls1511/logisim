7989b25dc499f9ff36a78f462ee63a928ac179ac
nGyu <vpdls1511@gmail.com>
2025-05-30 23:40:35 +0900

    test : 커맨드 객체 검증 단위 테스트 수정

--- src/test/java/com/logisim/product/ProductCommandTest.java
+++ src/test/java/com/logisim/product/ProductCommandTest.java
@@ -3,6 +3,7 @@ package com.logisim.product;
 import com.logisim.product.application.command.ProductCreateCommand;
 import com.logisim.product.adapter.in.api.dto.ProductCreateRequest;
 import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
@@ -31,44 +33,37 @@ public class ProductCommandTest {
   @Test
   @DisplayName("[FAIL] name 공백 테스트")
   void toCommandNameFail() {
+    //given
     String unit = "개";
     String name = "";
     String description  = "2000자넘기는 테스트도 해보고싶은데 이거너무길다 ㅎㅎ..";
-
     ProductCreateRequest request = new ProductCreateRequest(unit, name, description);
 
-    Set<ConstraintViolation<ProductCreateCommand>> violations = validator.validate(request.toCommand());
-    Iterator<ConstraintViolation<ProductCreateCommand>> iterator = violations.iterator();
-    List<String> messages = new ArrayList<>();
+    //when
+    Throwable throwable = catchThrowable(() -> validator.validate(request.toCommand()));
 
-    while (iterator.hasNext()) {
-      ConstraintViolation<ProductCreateCommand> violation = iterator.next();
-      messages.add(violation.getMessage());
-    }
-
-    assertThat(messages).contains("공백일 수 없습니다");
+    //then
+    assertThat(throwable)
+      .isInstanceOf(ConstraintViolationException.class)
+      .hasMessageContaining("name");
   }
 
 
   @Test
   @DisplayName("[FAIL] name 공백 테스트")
   void toCommandUnitFail() {
+    // given
     String unit = "";
     String name = "이 상품은 테스트 상품 입니다.";
     String description  = "2000자넘기는 테스트도 해보고싶은데 이거너무길다 ㅎㅎ..";
-
     ProductCreateRequest request = new ProductCreateRequest(unit, name, description);
 
-    Set<ConstraintViolation<ProductCreateCommand>> violations = validator.validate(request.toCommand());
-    Iterator<ConstraintViolation<ProductCreateCommand>> iterator = violations.iterator();
-    List<String> messages = new ArrayList<>();
+    //when
+    Throwable throwable = catchThrowable(() -> validator.validate(request.toCommand()));
 
-    while (iterator.hasNext()) {
-      ConstraintViolation<ProductCreateCommand> violation = iterator.next();
-      messages.add(violation.getMessage());
-    }
-
-    assertThat(messages).contains("공백일 수 없습니다");
+    assertThat(throwable)
+      .isInstanceOf(ConstraintViolationException.class)
+      .hasMessageContaining("unit");
   }
 
 
